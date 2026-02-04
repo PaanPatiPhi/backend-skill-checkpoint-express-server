513d96dc8b9e2515e294b792e98102b3a91d66cf
Phi <ptp.thamboonrak@gmail.com>
2026-02-04 14:35:59 +0700

    fix: enhance validation for answer content length in middleware

--- middlewares/validateCreateAnswer.mjs
+++ middlewares/validateCreateAnswer.mjs
@@ -2,7 +2,7 @@ export const validateCreateAnswer = (req, res, next) => {
   const { content } = req.body;
 
   // โจทย์ไม่ได้กำหนดรายละเอียด → เช็คแค่มี / ไม่มี
-  if (!content || typeof content !== "string" || content.trim() === "") {
+  if (!content || typeof content !== "string" || content.trim() === "" || content.length > 300 ) {
     return res.status(400).json({
       message: "Invalid request data."
     });
